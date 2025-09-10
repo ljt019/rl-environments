f4d3a00e0531954d1f10f605b2676ea964d50b0c
Lucien Thomas <lucienthomas00@gmail.com>
2025-09-10 01:56:06 -0500

    tad

--- environments/battleship/scripts/grpo.py
+++ environments/battleship/scripts/grpo.py
@@ -15,7 +15,6 @@ model, tokenizer = vf.get_model_and_tokenizer(MODEL_NAME)
 
 args = vf.grpo_defaults(run_name=RUN_NAME)
 
-# Battleship games are longer than average, increase max prompt length and timeout
 args.max_prompt_length = 2048
 args.async_generation_timeout = 1200.0
 args.max_seq_len = 52_224
