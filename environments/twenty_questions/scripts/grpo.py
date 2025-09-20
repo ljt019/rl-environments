c01734f47d93498d2a483993e87ba57576b797ad
Lucien Thomas <lucienthomas00@gmail.com>
2025-09-19 20:28:33 -0500

    tweak

---
+++ environments/twenty_questions/scripts/grpo.py
@@ -0,0 +1,32 @@
+import os
+
+import verifiers as vf
+
+from environments.twenty_questions.twenty_questions import load_environment
+
+############## Training Config ##############
+
+MODEL_NAME = "willcb/Qwen3-1.7B"
+
+RUN_NAME = "twenty-questions-grpo-190925"
+
+#############################################
+
+env = load_environment(
+    answerer_model="gpt-4.1-nano",
+    answerer_base_url="https://openrouter.ai/api/v1",
+    answerer_api_key=os.getenv("OPENROUTER_API_KEY"),
+)
+
+model, tokenizer = vf.get_model_and_tokenizer(MODEL_NAME)
+
+args = vf.grpo_defaults(run_name=RUN_NAME)
+
+trainer = vf.GRPOTrainer(
+    model=model,
+    processing_class=tokenizer,
+    env=env,
+    args=args,
+)
+
+trainer.train()
