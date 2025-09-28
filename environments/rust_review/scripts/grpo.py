7d59697e3c55b6393a2fc5c5863754a876783485
Lucien Thomas <lucienthomas00@gmail.com>
2025-09-28 03:29:42 -0500

    tweak

--- environments/rust_review/scripts/grpo.py
+++ environments/rust_review/scripts/grpo.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import verifiers as vf
 from environments.rust_review.rust_review import load_environment
 
@@ -6,7 +8,7 @@ from environments.rust_review.rust_review import load_environment
 MODEL_NAME = "Qwen/Qwen2.5-Coder-7B-Instruct"
 REVIEW_APPLICATOR_MODEL = "openai/gpt-4.1-nano"
 
-RUN_NAME = "rust-review-grpo-270925"
+RUN_NAME = "rust-review-grpo-7b"
 
 #############################################
 
@@ -14,7 +16,7 @@ env = load_environment(review_applicator_model=REVIEW_APPLICATOR_MODEL)
 
 model, tokenizer = vf.get_model_and_tokenizer(MODEL_NAME)
 
-args = vf.grpo_defaults(run_name=RUN_NAME)
+args = vf.grpo_defaults(run_name=f"{RUN_NAME}-{datetime.now().strftime('%Y%m%d%H%M%S')}")
 args.max_steps = 2500
 args.save_steps = 25
 args.save_only_model = False
