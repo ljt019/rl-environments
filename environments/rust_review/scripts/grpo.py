043037878f6f034c44fefab9796b17c1ae654308
Lucien Thomas <lucienthomas00@gmail.com>
2025-09-28 02:44:38 -0500

    test

--- environments/rust_review/scripts/grpo.py
+++ environments/rust_review/scripts/grpo.py
@@ -1,7 +1,22 @@
 import verifiers as vf
 
 vf.setup_logging(level="DEBUG")
+from transformers import TrainerCallback
+
 from environments.rust_review.rust_review import load_environment
+
+
+class PushToHubOnSaveCallback(TrainerCallback):
+    def on_save(self, args, state, control, **kwargs):
+        trainer = kwargs["trainer"]
+        if state.is_world_process_zero:
+            trainer.push_to_hub(
+                commit_message=f"checkpoint-{state.global_step}",
+                blocking=True,
+                max_shard_size="15GB",
+            )
+        return control
+
 
 ############## Training Config ##############
 
@@ -18,14 +33,17 @@ model, tokenizer = vf.get_model_and_tokenizer(MODEL_NAME)
 
 args = vf.grpo_defaults(run_name=RUN_NAME)
 args.max_steps = 2500
-args.save_steps = 500
-args.logging_steps = 10
+args.save_steps = 2
+args.save_only_model = False
 
 trainer = vf.GRPOTrainer(
     model=model,
     processing_class=tokenizer,
+    hub_model_id="ljt019/Qwen2.5-Coder-7B-rust-review",
     env=env,
     args=args,
 )
+
+trainer.add_callback(PushToHubOnSaveCallback())
 
 trainer.train()
